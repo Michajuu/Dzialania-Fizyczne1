65161ccaf30b8d89498abaa99c794a3a07773754
Michajuu <73675435+Michajuu@users.noreply.github.com>
2021-04-21 07:24:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function Oblicz()
+{
+    let m = document.getElementById("m").value;
+    let γ = document.getElementById("γ").value;
+let a = document.getElementById("Q");
+a.innerHTML = m * γ;
+}
+function powrot() {
+    let b = location.href;
+    b = "https://michajuu.github.io/Dzialania-Fizyczne/";
+  }
